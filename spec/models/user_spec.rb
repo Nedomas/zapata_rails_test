ab6196361f387be08a1d02c0162cded5f97daa93
Domas <domas.bitvinskas@me.com>
2014-08-08 20:22:44 +0300

    User spec pass

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,3 +3,7 @@ require '/Users/domas/Developer/zapata_rails_test/spec/rails_helper'
 describe User do
 
   it '#find' do
+    expect(User.find('Missing "id"')).to eq('Returned instance object User')
+  end
+
+end
